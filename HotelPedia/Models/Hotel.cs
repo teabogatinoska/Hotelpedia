5af66329aecc341312daf48977a6e7df9dc7ccb0
Богатиноска Теа <tea.bogatinoska@students.finki.ukim.mk>
2020-12-14 20:42:03 +0100

    Hotel model

--- HotelPedia/Models/Hotel.cs
+++ HotelPedia/Models/Hotel.cs
@@ -7,5 +7,29 @@ namespace HotelPedia.Models
 {
     public class Hotel
     {
+        public long id { get; set; }
+        public string name_mk { get; set; }
+        public string name_ang { get; set; }
+        public int starsNum { get; set; }
+        public double price { get; set; }
+        public bool breakfast_included { get; set; }
+        public bool free_cancellation { get; set; }
+        public bool swimming_pool { get; set; }
+        public bool parking { get; set; }
+        public bool wifi { get; set; }
+        public bool smoking { get; set; }
+        public bool disabled_guest { get; set; }
+        public bool pets { get; set; }
+        public double distance_center { get; set; }
+        public double distance_airport { get; set; }
+        public string street { get; set; }
+        public long postcode { get; set; }
+        public string city { get; set; }
+        public string website { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+
     }
+
+ 
 }
