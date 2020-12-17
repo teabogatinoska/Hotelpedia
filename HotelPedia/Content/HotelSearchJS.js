058e03e6727988a9a6c27d15fd9a46c5a2360483
LjubenaAngelovska <ljubenaa@gmail.com>
2020-12-17 02:02:40 +0100

    Sort JavaScript added

---
+++ HotelPedia/Content/HotelSearchJS.js
@@ -0,0 +1,64 @@
+﻿
+function funcSort() {
+
+    var arrHotels = document.getElementsByClassName('oneHotel');
+    var arrZaSort = [];
+
+    var sortOptions = document.getElementById('sortSelect');
+    var sortChoice = sortOptions.value;
+
+    for (var i = 0; i < arrHotels.length; i++) {
+
+        if (sortChoice === "most_stars") {
+            var child = arrHotels[i].querySelectorAll('.brStars');
+            var sorterBy = 0 - child[0].textContent;
+        }
+        else if (sortChoice === "least_stars") {
+            var child = arrHotels[i].querySelectorAll('.brStars');
+            var sorterBy = child[0].textContent;
+        }
+        else if (sortChoice === "lowest_price") {
+            var child = arrHotels[i].querySelectorAll('.brPrice');
+            var sorterBy = child[0].textContent;
+        }
+        else if (sortChoice === "highest_price") {
+            var child = arrHotels[i].querySelectorAll('.brPrice');
+            var sorterBy = 0 - child[0].textContent;
+        }
+        else if (sortChoice === "center_closest") {
+            var child = arrHotels[i].querySelectorAll('.distCenter');
+            var sorterBy = child[0].textContent;
+        }
+        else if (sortChoice === "airport_closest") {
+            var child = arrHotels[i].querySelectorAll('.distAirport');
+            var sorterBy = child[0].textContent;
+        }
+        else if (sortChoice === "az") {
+            var child = arrHotels[i].querySelectorAll('.imeAngHotel');
+            var sorterBy = child[0].textContent;
+        }
+        else {
+            var child = arrHotels[i].querySelectorAll('.imeAngHotel');
+            var sorterBy = child[0].textContent;
+        }
+
+        arrZaSort.push([sorterBy, arrHotels[i]]);
+    }
+
+    arrZaSort.sort(function (a, b) {
+
+        if (a[0] === b[0]) {
+            return 0;
+        }
+        else {
+            return (a[0] < b[0]) ? -1 : 1;
+        }
+
+    });
+
+    console.log(arrZaSort);
+
+
+
+
+}
